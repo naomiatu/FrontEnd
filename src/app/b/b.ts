00595495a4502fb948c6b182573cdb965a308d7a
naomi <g00470124@atu.ie>
2026-02-22 22:33:21 +0000

    added comments for clarity
    Added comments to clarify component usage and structure.

--- src/app/b/b.ts
+++ src/app/b/b.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-b',
-  imports: [],
-  templateUrl: './b.html',
-  styleUrl: './b.css',
+  selector: 'app-b',        // you need to use <app-b></app-b> in HTML to show this component
+  templateUrl: './b.html',  // this is you add layout/markup 
+  styleUrl: './b.css',      // component-specific styles go here
 })
 export class B {
-
+  //  variables and functions go here
 }
