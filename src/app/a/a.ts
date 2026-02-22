79a591993453cc9a3c965d830c5210f3c9b72764
naomi <g00470124@atu.ie>
2026-02-22 22:38:16 +0000

    added comments to component metadata in a.ts

--- src/app/a/a.ts
+++ src/app/a/a.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-a',
+  selector: 'app-a', // you use <app-a></app-a> in HTML to show this component
   imports: [],
-  templateUrl: './a.html',
-  styleUrl: './a.css',
+  templateUrl: './a.html',//where your layout/markup lives
+  styleUrl: './a.css',//component-specific styles here
 })
-export class A {
+export class A {  // variables and functions go here
 
 }
+
